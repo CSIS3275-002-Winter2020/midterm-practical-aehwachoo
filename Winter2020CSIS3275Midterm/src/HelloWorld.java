37dbed2642f3481cafcb174f79e0bb176b867f4b
aehwachoo <chooa1@student.douglascollege.ca>
2020-02-14 13:33:28 -0800

    Add name and email

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("name: Aehwa choo");
+		System.out.println("email: chooa1@student.douglascollege.ca");
+	}
+
+}
